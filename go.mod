53f5f0f5a5131436853d6ca5752d6cbfadb0ef91
Lukas Jürs Schmidt <mr.lukas.schmidt@gmail.com>
2021-03-25 23:45:40 +0100

    VED IKKE OM JEG HAR ØDELAGTE MIT GO?
    AHHHH! KAN IKKE TESTE EFTER JEG VED IKKE HVOR LANG TID!
    
    PlatformError: Cocoa: Failed to find service port for display..
    Ved ikke om noget virker mere :(

--- go.mod
+++ go.mod
@@ -2,4 +2,7 @@ module github.com/taxidriver2192/FirstGoGame
 
 go 1.16
 
-require github.com/hajimehoshi/ebiten/v2 v2.0.6 // indirect
+require (
+	github.com/hajimehoshi/ebiten v1.12.9
+	github.com/hajimehoshi/ebiten/v2 v2.0.6 // indirect
+)
--- main.go
+++ main.go
@@ -1,38 +1,111 @@
 package main
 
-import "github.com/hajimehoshi/ebiten/v2"
+import (
+	"image/color"
+	"time"
 
-type Game struct{}
+	"github.com/hajimehoshi/ebiten/v2"
+	"github.com/hajimehoshi/ebiten/v2/ebitenutil"
+	"github.com/hajimehoshi/ebiten/v2/inpututil"
+)
+
+type Dir int
+
+type Game struct {
+	moveDirection int
+	snakeBody     []Position
+	timer         int
+	moveTime      int
+}
 
 // Create constants we can use continuously
 const (
-	screenWidth  = 512
-	screenHeight = 512
+	screenWidth  = 500
+	screenHeight = 500
+	gridSize     = 25
+	xNumInScreen = screenWidth / gridSize
+	yNumInScreen = screenHeight / gridSize
 )
 
-var (
-	White = Gray16{0xffff}
+const (
+	dirNone = iota
+	dirLeft
+	dirRight
+	dirDown
+	dirUp
 )
+
+type Position struct {
+	X int
+	Y int
+}
+
+func (g *Game) needsToMoveSnake() bool {
+	return g.timer%g.moveTime == 0
+}
 
 func (g *Game) Update() error {
-	// Update the logical stage
+	if g.needsToMoveSnake() {
+		if inpututil.IsKeyJustPressed(ebiten.KeyD) {
+			g.moveDirection = dirLeft
+		}
+		if inpututil.IsKeyJustPressed(ebiten.KeyA) {
+			g.moveDirection = dirRight
+		}
+		if inpututil.IsKeyJustPressed(ebiten.KeyS) {
+			g.moveDirection = dirDown
+		}
+		if inpututil.IsKeyJustPressed(ebiten.KeyW) {
+			g.moveDirection = dirUp
+		}
+		for i := int64(len(g.snakeBody)) - 1; i > 0; i-- {
+			g.snakeBody[i].X = g.snakeBody[i-1].X
+			g.snakeBody[i].Y = g.snakeBody[i-1].Y
+		}
+		switch g.moveDirection {
+		case dirLeft:
+			g.snakeBody[0].X--
+		case dirRight:
+			g.snakeBody[0].X++
+		case dirDown:
+			g.snakeBody[0].Y++
+		case dirUp:
+			g.snakeBody[0].Y--
+		}
+	}
+	time.Sleep(300)
+	g.timer++
 	return nil
 }
 
+// Dir returns a currently pressed direction.
+// Dir returns false if no direction key is pressed.
+
 func (g *Game) Draw(screen *ebiten.Image) {
-	// Render the screen
+	for _, v := range g.snakeBody {
+		ebitenutil.DrawRect(screen, float64(v.X*gridSize), float64(v.Y*gridSize), gridSize, gridSize, color.RGBA{0x80, 0xa0, 0xc0, 0xff})
+	}
+}
+
+func newGame() *Game {
+	g := &Game{
+		snakeBody: make([]Position, 1),
+		//moveDirection
+	}
+	g.snakeBody[0].X = xNumInScreen / 2
+	g.snakeBody[0].Y = yNumInScreen / 2
+	return g
 }
 
 func (g *Game) Layout(outsideWidth, outsideHeight int) (int, int) {
-	// Return the game screen size
 	return screenWidth, screenHeight
 }
 
 func main() {
-	ebiten.SetWindowSize(screenWidth, screenHeight)
-	ebiten.SetWindowTitle("Snake")
-	game := &Game{}
-	if err := ebiten.RunGame(game); err != nil {
+	//ebiten.SetWindowSize(screenWidth, screenHeight)
+	ebiten.SetWindowTitle("Snake (Ebiten Demo)")
+	//game := &Game{}
+	if err := ebiten.RunGame(newGame()); err != nil {
 		panic(err)
 	}
 }
